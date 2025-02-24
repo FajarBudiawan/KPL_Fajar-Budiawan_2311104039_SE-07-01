f60416f1d3a66ec661ebc6701ea4bc7526c5b9df
FajarBudiawan <you@example.com>
2025-02-24 12:13:15 +0700

    commit ke empat

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_Nodejs/variabel-local-global.js
@@ -0,0 +1,12 @@
+let globalVariabel = "Agustinus mahardika";
+
+function getMyIdentity() {
+  let localVariabel = 2311104039;
+
+  console.log(`Nama saya ${globalvariabel} nim ${localVariabel}`);
+}
+
+console.log(globalVariabel);
+// console.log(localVariabel);
+
+getMyIdentity;
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_Nodejs/variabel.js
@@ -0,0 +1,15 @@
+//variabel var
+var name = "Fajar budiawan";
+//variabel const
+const nim = 2311104039;
+console.log(name);
+
+console.log("nama saya adalah" + name + "dan nim saya adalah" + nim);
+console.log(`nama saya adalah ${name} dan nim saya adalah ${nim}`);
+
+console.log(`nama saya adalah ${name} 
+    dan nim saya adalah ${nim}`);
+
+//variabel let
+let fullname = "AGUSTINUS MAHARDIKA";
+console.log(fullname);
